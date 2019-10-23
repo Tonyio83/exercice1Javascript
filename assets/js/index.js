fee32bd9b78fb4832c2395452a500a4d64d1db5b
Tonyio83 <anthonyfrancelle@gmail.com>
2019-10-23 10:06:50 +0200

    exercice 3 fini

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,1 @@
-var lastName = "Doe";
-var firstName = "John"
-var city = "New York"
-alert("Nom : " + lastName +
-"\nPrénom : " + firstName +
-"\nVille : " + city);
+let Prénom = prompt("Bonjour,");
